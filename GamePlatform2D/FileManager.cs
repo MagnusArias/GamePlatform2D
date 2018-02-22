a490bc7b399aedbcdcf88425cac28e3eb1cd1e6c
Przemysław Dębiec <194933@edu.p.lodz.pl>
2018-02-22 21:11:48 +0100

    Tutorial 9 Finished

--- GamePlatform2D/FileManager.cs
+++ GamePlatform2D/FileManager.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace GamePlatform2D
 {
@@ -13,7 +7,124 @@ namespace GamePlatform2D
     {
         enum LoadType { Attributes, Contents };
 
+        LoadType type;
+
         List<List<string>> attributes = new List<List<string>>();
         List<List<string>> contents = new List<List<string>>();
+
+        List<string> tempAttributes;
+        List<string> tempContents;
+
+        bool identifierFound = false;
+
+        public void LoadContent(string filename, List<List<string>> attributes, List<List<string>> contents)
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (line.Contains("Load="))
+                    {
+                        tempAttributes = new List<string>();
+                        line.Remove(0, line.IndexOf("=") + 1);
+                        type = LoadType.Attributes;
+                    }
+                    else
+                    {
+                        tempContents = new List<string>();
+                        type = LoadType.Contents;
+                    }
+
+                    string[] lineArray = line.Split(']');
+                    foreach (string li in lineArray)
+                    {
+                        string newLine = line.Trim('[', ' ', ']');
+                        if (newLine != string.Empty)
+                        {
+                            if (type == LoadType.Contents)
+                                tempContents.Add(newLine);
+                            else
+                                tempAttributes.Add(newLine);
+                        }
+                    }
+
+                    if (type == LoadType.Contents && tempContents.Count > 0)
+                    {
+                        contents.Add(tempContents);
+                        attributes.Add(tempAttributes);
+                    }
+                }
+            }
+
+        }
+
+        public void LoadContent(string filename, List<List<string>> attributes, List<List<string>> contents, string identifier)
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (line.Contains("EndLoad=") && line.Contains(identifier))
+                    {
+                        identifierFound = false;
+                        break;
+                    }
+                    else if (line.Contains("Load=") && line.Contains(identifier))
+                    {
+                        identifierFound = true;
+                        continue;
+                    }
+
+                    if (identifierFound)
+                    {
+                        if (line.Contains("Load="))
+                        {
+                            tempAttributes = new List<string>();
+                            line.Remove(0, line.IndexOf("=") + 1);
+                            type = LoadType.Attributes;
+                        }
+                        else
+                        {
+                            tempContents = new List<string>();
+                            type = LoadType.Contents;
+                        }
+
+                        string[] lineArray = line.Split(']');
+                        foreach (string li in lineArray)
+                        {
+                            string newLine = line.Trim('[', ' ', ']');
+                            if (newLine != string.Empty)
+                            {
+                                if (type == LoadType.Contents)
+                                    tempContents.Add(newLine);
+                                else
+                                    tempAttributes.Add(newLine);
+                            }
+                        }
+
+                        if (type == LoadType.Contents && tempContents.Count > 0)
+                        {
+                            contents.Add(tempContents);
+                            attributes.Add(tempAttributes);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
+
+
+/*
+ * Load=[Image]
+ * [splash/image]
+ * [splasg/image2]
+ * 
+ * EndLoad=[Image]
+ * 
+ *
+ */
